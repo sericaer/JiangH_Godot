28cec53ee61986381a59f65c109aef148a5993f2
sericaer <sericaer@outlook.com>
2021-10-13 20:35:03 +0800

    显示中文

--- Native/mods/native/Main.gd
+++ Native/mods/native/Main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var gmData
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,9 +8,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Player.text = "2222"
+	pass
 
-
+func set_game_object(gm : Object):
+	gmData = gm
+	$Player.text = gm.player.name
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
